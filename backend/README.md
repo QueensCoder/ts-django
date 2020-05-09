c0e01e4961586a1270932440ad859b3a00eee04f
Ozal Khan <ozalkhan196@gmail.com>
2020-05-09 17:51:12 -0400

    python interpreter works with pipenv shell, had to update mac os

---
+++ backend/README.md
@@ -0,0 +1,13 @@
+### django cheat sheet
+
+## how tostart project
+
+pipenv shell
+
+pipenv install django
+
+django-admin startproject <project name>
+
+cd project_folder
+
+python manage.py run server - runs server
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'view_demo',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre',
+        'USER': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
