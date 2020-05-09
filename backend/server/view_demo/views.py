f93528ee6cb581b10b55ba434652849582d7e312
Ozal Khan <ozalkhan196@gmail.com>
2020-05-09 19:11:44 -0400

    views hooked up the work with view_demo, able to server html from views using template engine, updated readme, ready to move to rest framework

--- backend/server/view_demo/views.py
+++ backend/server/view_demo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todos
 
-# Create your views here.
+
+def test(req):
+    return HttpResponse('<h1>Hello world</h1>')
+
+
+def index(req):
+    todos = Todos.objects.all()
+    return render(req, 'new/index.html', {})
